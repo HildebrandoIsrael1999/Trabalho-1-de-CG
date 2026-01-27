446130527aff6f4f0a1537dff30e7b5d35e17ed1
Hil32831081@ <Hildebrando.sales@aluno.uece.br>
2026-01-27 00:09:12 -0300

    Ajustando música e novo fluxo para main

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ from interface import executar_menu_principal
 from config import criar_estado_inicial, processar_eventos_jogo, atualizar_estado_jogo, desenhar_jogo
 
 pygame.init()
+pygame.mixer.init() 
+
 pygame.display.set_caption("Billy da Tapioca")
 clock = pygame.time.Clock()
 largura, altura = 1280, 720
 tela = pygame.display.set_mode((largura, altura))
 
 
+path_menu = r"C:\Users\hildebrando.sales\Downloads\Menu.mpeg"
+path_jogo = r"C:\Users\hildebrando.sales\Downloads\game-gaming-video-game-music-459876.mp3"
+
+
+try:
+    pygame.mixer.music.load(path_menu)
+    pygame.mixer.music.play(-1) 
+except pygame.error:
+    print("Erro ao carregar música do menu")
+
+
 if not executar_menu_principal(tela, largura, altura):
     pygame.quit()
     sys.exit()
+
+try:
+    pygame.mixer.music.stop()        
+    pygame.mixer.music.load(path_jogo) 
+    pygame.mixer.music.play(-1)      
+except pygame.error:
+    print("Erro ao carregar música do jogo")
 
 estado_jogo = criar_estado_inicial(largura, altura)
 
@@ -24,9 +44,7 @@ while rodando:
             rodando = False
     
     processar_eventos_jogo(estado_jogo, eventos)
-
     atualizar_estado_jogo(estado_jogo)
-
     desenhar_jogo(tela, estado_jogo)
 
     pygame.display.flip()
